6e345c87be0a2e9277165fc855a7cd158ad2db79
Nadir Hossain <nadirhossain336@gmail.com>
2024-06-25 17:20:04 +0600

    :art: complete

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
 export const baseURL = axios.create({
-    baseURL: "http://192.168.10.16:5001/api/v1",
+    // baseURL: "http://192.168.10.16:5001/api/v1",
+    baseURL: "http://209.38.136.64:5000/api/v1",
     timeout: 10000,
     headers: {'X-Custom-Header': 'foobar'}
 });
-export const ImageConfig = "http://192.168.10.16:5001"
-export const socketURL = "http://192.168.10.16:5001";
+// export const ImageConfig = "http://192.168.10.16:5001"
+export const ImageConfig = "http://209.38.136.64:5000"
+// export const socketURL = "http://192.168.10.16:5001";
+export const socketURL = "http://209.38.136.64:5000";
